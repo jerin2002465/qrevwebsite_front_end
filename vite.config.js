95c59b1d4156b3cba03824006ec27f87648ac74b
jerin2002465 <jerinsujith.scode@gmail.com>
2025-09-05 18:14:16 +0530

    changed-vercel-json

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
   server: {
     host: "0.0.0.0", // Allow access from local IP
     port: 5173, // Optional: change if needed
+  },
+  build: {
+    outDir: "dist", // default is dist, ensure Vercel deploys from here
   },
 });
